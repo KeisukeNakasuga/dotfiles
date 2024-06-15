e4da61e80de760c682478d30a2a22d6e7f396022
KeisukeNakasuga <keisuke.nakasuga@gmail.com>
2024-06-15 16:33:26 +0900

    feat: add wezterm settings

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,9 +6,14 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
-config.color_scheme = 'MaterialDesignColors'
+config.check_for_updates = true
+config.check_for_updates_interval_seconds = 86400
+-- config.color_scheme = "Dracula (Official)"
+-- config.color_scheme = "Dracula (Gogh)"
+config.color_scheme = "Dracula+"
 config.font_size = 11
 config.window_background_opacity = 0.75
+config.scrollback_lines = 10000
 
 local act = wezterm.action
 config.keys = {
