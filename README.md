46284e6bcb5f6c394e6ccdb38136c811fe898b48
KeisukeNakasuga <keisuke.nakasuga@gmail.com>
2024-06-25 02:22:08 +0900

    feat: add install script

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # dotfiles
 My dotfiles.
+
+```bash
+bash ./install.sh
+```
---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+current_dir=$(pwd)
+home_dir=$HOME
+
+# install .wezterm.lua
+source_path="$current_dir/.wezterm.lua"
+target_path="$home_dir/.wezterm.lua"
+if [ -L "$target_path" ]; then
+  echo "$target_path is already exist."
+else
+  ln -s "$source_path" "$target_path"
+  echo "create symbolic link: $target_path -> $source_path"
+fi
