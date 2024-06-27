36a1281d22e752f681bdd96cfb7f1ce2a3a64851
KeisukeNakasuga <keisuke.nakasuga@gmail.com>
2024-06-28 03:26:31 +0900

    feat: add Xmodmap settings

--- install.sh
+++ install.sh
@@ -12,3 +12,13 @@ else
   ln -s "$source_path" "$target_path"
   echo "create symbolic link: $target_path -> $source_path"
 fi
+
+# .Xmodmap
+source_path="$current_dir/.Xmodmap"
+target_path="$home_dir/.Xmodmap"
+if [ -L "$target_path" ]; then
+  echo "$target_path is already exist."
+else
+  ln -s "$source_path" "$target_path"
+  echo "create symbolic link: $target_path -> $source_path"
+fi
