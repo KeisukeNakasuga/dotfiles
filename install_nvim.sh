58b7df4196a9398b537bdff806762c63d182145e
KeisukeNakasuga <keisuke.nakasuga@gmail.com>
2024-11-25 01:23:52 +0900

    feat: neovimをsnap経由でインストールするように修正

--- install_nvim.sh
+++ install_nvim.sh
@@ -7,7 +7,7 @@ source_path="$current_dir/nvim"
 target_path="$home_dir/.config/nvim"
 if ! command -v nvim &> /dev/null
 then
-  sudo apt -y install neovim
+  sudo snap -y install nvim
 else
   ln -s "$source_path" "$target_path"
   echo "create symbolic link: $target_path -> $source_path"
