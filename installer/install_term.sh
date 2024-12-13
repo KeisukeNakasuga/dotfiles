145e8902ca98d54a843dcae4bef81a3502c8bd93
KeisukeNakasuga <keisuke.nakasuga@gmail.com>
2024-12-14 02:14:01 +0900

    feat: zsh追加
    シェルはzshを使う

--- installer/install_term.sh
+++ installer/install_term.sh
@@ -3,6 +3,20 @@
 current_dir=$(pwd)
 home_dir=$HOME
 
+# zsh & Powerlevel10k & zsh-syntax-highlighting
+if ! command -v zsh &> /dev/null; then
+  sudo apt -y install zsh
+  # Powerlevel10k
+  sudo git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /opt/powerlevel10k
+  echo 'source /opt/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+  # zsh-syntax-highlight
+  sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /opt/zsh-syntax-highlighting
+  echo "source /opt/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+fi
+zsh_version=$(zsh --version)
+echo "zsh version: $zsh_version"
+
+
 # wezterm
 source_path="$current_dir/term/.wezterm.lua"
 target_path="$home_dir/.wezterm.lua"
