414e18d65b4040e94d809bf501c9743b54f61ac2
KeisukeNakasuga <keisuke.nakasuga@gmail.com>
2024-12-13 23:16:59 +0900

    feat: lazygit追加
    neovimのプラグインにlazygitを追加
    neovimプラグインのアップデート

--- installer/install_nvim.sh
+++ installer/install_nvim.sh
@@ -33,3 +33,15 @@ fi
 
 neovide_version=$(neovide --version)
 echo "Neovide version: $neovide_version"
+
+
+# lazygit
+if ! command -v lazygit &> /dev/null; then
+  LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+  curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+  tar xf lazygit.tar.gz lazygit
+  sudo install lazygit -D -t /usr/local/bin/
+fi
+
+lazygit_version=$(lazygit --version)
+echo "Lazygit version: $lazygit_version"
