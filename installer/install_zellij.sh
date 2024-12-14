ab52beafc81906a9e6f3d639a787746f360cdc07
KeisukeNakasuga <keisuke.nakasuga@gmail.com>
2024-12-14 10:49:53 +0900

    feat: zellijの設定を追加
    zellizの設定ファイルを追加

--- installer/install_zellij.sh
+++ installer/install_zellij.sh
@@ -6,3 +6,14 @@ fi
 
 zellij_version=$(zellij --version)
 echo "zellij version: $zellij_version"
+
+current_dir=$(pwd)
+home_dir=$HOME
+source_path="$current_dir/term/zellij/config.kdl"
+target_path="$home_dir/.config/zellij/config.kdl"
+if [ -L "$target_path" ]; then
+  echo "$target_path already exist."
+else
+  ln -s "$source_path" "$target_path"
+  echo "create symbolic link: $target_path -> $source_path"
+fi
