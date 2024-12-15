8f4521c18a1c0c5d7933a8f72e6cdedf7e6e3970
KeisukeNakasuga <keisuke.nakasuga@gmail.com>
2024-12-16 03:59:54 +0900

    feat: ripgrepをインストール

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/bash
 
-sudo apt -y update && sudo apt -y upgrade
-
 chmod +x ./installer/*.sh
+./installer/install_core.sh
 ./installer/install_rust.sh
 ./installer/install_node.sh
 ./installer/install_term.sh
