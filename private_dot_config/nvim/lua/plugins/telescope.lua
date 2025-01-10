435561bc035561fc0fc74ffc152251ec6260bbce
KeisukeNakasuga <keisuke.nakasuga@gmail.com>
2025-01-10 13:05:12 +0900

    feat: telescopeの設定を修正
    ドットファイル内の検索を可能にする(.gitは除く)

--- private_dot_config/nvim/lua/plugins/telescope.lua
+++ private_dot_config/nvim/lua/plugins/telescope.lua
@@ -3,22 +3,36 @@ return {
   dependencise = {
     'nvim-lua/plenary.nvim'
   },
+  opts = {
+    defaults = {
+      vimgrep_arguments = {
+        "rg",
+        "--color=never",
+        "--no-heading",
+        "--with-filename",
+        "--line-number",
+        "--column",
+        "--smart-case",
+        "--hidden",
+      },
+      mappings = {
+        i = {
+          ['<C-u>'] = false,
+          ['<C-d>'] = false,
+        },
+      },
+    },
+  },
   keys = {
     { '<leader>ff', function() require('telescope.builtin').find_files() end, desc = 'Telescope find files' },
-    { '<leader>fg', function() require('telescope.builtin').live_grep() end, desc = 'Telescope live grep' },
+    { '<leader>fg', function()
+        require('telescope.builtin').live_grep({
+          cwd = vim.fn.getcwd(),
+          additional_args = function() return { "--glob", "!.git" } end
+        })
+      end, desc = 'Telescope live grep'
+    },
     { '<leader>fb', function() require('telescope.builtin').buffers() end, desc = 'Telescope buffers' },
     { '<leader>fh', function() require('telescope.builtin').help_tags() end, desc = 'Telescope help tags' },
   },
-  config = function ()
-    require('telescope').setup({
-      defaults = {
-        mappings = {
-          i = {
-            ['<C-u>'] = false, -- Ctrl+u を無効化
-            ['<C-d>'] = false, -- Ctrl+d を無効化
-          },
-        },
-      },
-    })
-  end
 }
