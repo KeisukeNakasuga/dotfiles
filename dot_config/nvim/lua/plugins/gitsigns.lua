21d95642796c58771b99b8de5e0f4bfd3660fbc2
KeisukeNakasuga <keisuke.nakasuga@gmail.com>
2025-03-29 13:48:02 +0900

    feat: blame設定を追加

--- dot_config/nvim/lua/plugins/gitsigns.lua
+++ dot_config/nvim/lua/plugins/gitsigns.lua
@@ -2,6 +2,15 @@ return {
   'lewis6991/gitsigns.nvim',
   opt = {},
   config = function ()
-    require('gitsigns').setup()
+    require('gitsigns').setup {
+      current_line_blame = true,
+      current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = 'eol',
+        delay = 100,
+        ignore_white_space = false,
+      }
+    }
+    vim.api.nvim_set_hl(0, "GitSignsCurrentLineBlame", { fg = "#bbbbbb", italic = true })
   end,
 }
