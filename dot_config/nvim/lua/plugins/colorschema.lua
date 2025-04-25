b542d204548d582ede670489508830a72f460944
KeisukeNakasuga <keisuke.nakasuga@gmail.com>
2025-04-25 15:56:22 +0900

    feat: alacritty, nvimの背景透過設定を追加

--- dot_config/nvim/lua/plugins/colorschema.lua
+++ dot_config/nvim/lua/plugins/colorschema.lua
@@ -1,20 +1,33 @@
 return {
+--   {
+--     'cocopon/iceberg.vim',
+--     lazy = false,
+--     priority = 1000,
+--     opts = {},
+--     config = function()
+--       vim.cmd([[colorscheme iceberg]])
+--     end,
+--   },
   {
-    'cocopon/iceberg.vim',
-    lazy = false,
-    priority = 1000,
-    opts = {},
-    config = function()
-      vim.cmd([[colorscheme iceberg]])
-    end,
-  },
-  --{
-  --    'folke/tokyonight.nvim',
-  --    lazy = false,
-  --    priority = 1000,
-  --    opts = {},
-  --    config = function()
-  --      vim.cmd([[colorscheme tokyonight-night]])
-  --    end,
-  --  }
+     'folke/tokyonight.nvim',
+     lazy = false,
+     priority = 1000,
+     opts = {
+      style = 'night',
+      transparent = true,
+      sidebars = 'transparent',
+      floats = 'transparent'
+    },
+     config = function()
+       vim.cmd([[colorscheme tokyonight-night]])
+       vim.cmd([[
+         highlight Normal guibg=NONE ctermbg=NONE
+         highlight NormalNC guibg=NONE ctermbg=NONE
+         highlight EndOfBuffer guibg=NONE ctermbg=NONE
+         highlight NvimTreeNormal guibg=NONE ctermbg=NONE
+         highlight NvimTreeNormalNC guibg=NONE ctermbg=NONE
+         highlight NvimTreeEndOfBuffer guibg=NONE ctermbg=NONE
+         ]])
+       end,
+   }
 }
