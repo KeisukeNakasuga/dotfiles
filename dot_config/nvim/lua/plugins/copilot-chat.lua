12893052a91dee8646cb8d23dff0394e85e11f5c
KeisukeNakasuga <keisuke.nakasuga@gmail.com>
2025-07-02 15:01:08 +0900

    feat: Copilotの設定を追加

---
+++ dot_config/nvim/lua/plugins/copilot-chat.lua
@@ -0,0 +1,13 @@
+return {
+  "CopilotC-Nvim/CopilotChat.nvim",
+  branch = "main",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+    },
+    build = "make tiktoken", -- Only on MacOS or Linux
+    opts = {
+      debug = true, -- Enable debugging
+      -- See Configuration section for rest
+    },
+}
