c5bcda39a1e3d648f69231bf7c5e9866b691ca14
KeisukeNakasuga <keisuke.nakasuga@gmail.com>
2025-07-09 13:11:02 +0900

    feat: 不要なコメントを削除

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -64,12 +64,8 @@ vim.api.nvim_create_autocmd('VimEnter', {
 
 require("lazy").setup({
   spec = {
-    -- import your plugins
     { import = "plugins" },
   },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { 'iceberg' } },
-  -- automatically check for plugin updates
   checker = { enabled = true },
 })
